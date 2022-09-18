a0c7cd472d0a304bf69bc726ef52c3e419ddf417
AbuRayhaan <91697183+AbuRayhaan@users.noreply.github.com>
2022-09-19 00:23:58 +0100

    Modified App File

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { Route, Routes } from 'react-router-dom';
 import Main from './pages/main';
 import Reserve from './pages/reserve';
 import Reservation from './pages/myreserve';
+import AddItem from './pages/add';
+import RemoveItem from './pages/remove';
 import Navbar from './components/navbar';
 
 function App() {
@@ -14,6 +16,8 @@ function App() {
           <Route path="/" element={<Main />} />
           <Route path="reserve" element={<Reserve />} />
           <Route path="MyReservation" element={<Reservation />} />
+          <Route path="AddItem" element={<AddItem />} />
+          <Route path="RemoveItem" element={<RemoveItem />} />
         </Routes>
       </main>
     </>
