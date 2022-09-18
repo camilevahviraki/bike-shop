ed885101a3420ed4af27fdf8183a07bdf9106932
AbuRayhaan <91697183+AbuRayhaan@users.noreply.github.com>
2022-09-18 23:05:29 +0100

    Added MyReservation Page Components

---
+++ src/components/myreserve/myreserve.js
@@ -0,0 +1,7 @@
+function ReservePage() {
+  return (
+    <h1>My Reservation Page</h1>
+  );
+}
+
+export default ReservePage;
---
+++ src/pages/myreserve.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReservePage from '../components/myreserve/myreserve';
+
+function Reserve() {
+  return (
+    <ReservePage />
+  );
+}
+
+export default Reserve;
