72d0f053cc526b9dbdfb9b85561c7679ba2e07ae
AbuRayhaan <91697183+AbuRayhaan@users.noreply.github.com>
2022-09-18 23:03:16 +0100

    Modified App/Index files

--- src/App.js
+++ src/App.js
@@ -1,28 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Main from './pages/main';
+import Reserve from './pages/reserve';
+import Reservation from './pages/myreserve';
+import Navbar from './components/navbar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <main>
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="reserve" element={<Reserve />} />
+          <Route path="MyReservation" element={<Reservation />} />
+        </Routes>
+      </main>
+    </>
   );
 }
 
---
+++ src/components/navbar.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import '../css/navbar.css';
+
+function Navbar() {
+  return (
+    <header>
+      <div className="navbar d-flex">
+        <div className="navTitle d-flex">
+          <h1>Bike Shop</h1>
+        </div>
+
+        <div className="navMenu d-flex">
+          <NavLink to="/" className={({ isActive }) => (isActive ? 'rocket active' : 'rocket link')}>
+            <span>Motorcycles</span>
+          </NavLink>
+
+          <NavLink to="Reserve" className={({ isActive }) => (isActive ? 'rocket active' : 'rocket link')}>
+            <span>Reserve</span>
+          </NavLink>
+
+          <NavLink to="MyReservation" className={({ isActive }) => (isActive ? 'rocket active' : 'rocket link')}>
+            <span>My Reservations</span>
+          </NavLink>
+
+          <NavLink to="Add-Motorcycle" className={({ isActive }) => (isActive ? 'rocket active' : 'rocket link')}>
+            <span>Add Item</span>
+          </NavLink>
+
+          <NavLink to="Remove-Motorcycle" className={({ isActive }) => (isActive ? 'rocket active' : 'rocket link')}>
+            <span>Remove Item</span>
+          </NavLink>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default Navbar;
