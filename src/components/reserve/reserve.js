2f8d44b44d3f7858f501eb09c6676dff6ec01960
camilevahviraki <camilevahviraki@gmail.com>
2022-09-23 00:53:15 +0200

    Update component/resrve

--- src/components/reserve/reserve.js
+++ src/components/reserve/reserve.js
@@ -1,6 +1,49 @@
+import {useDispatch, useSelector} from 'react-redux';
+import sampleData from '../main/sampleRespone.json';
+import { reserveMotorcycle } from '../../redux/main/motorcycles';
+import './reserve.css';
+
 function ReserveForm() {
+  const sampleResponse = useSelector(state => state.motorcyclesReducer)
+  const dispatch = useDispatch();
+  const reserve = (id) => {
+     dispatch(reserveMotorcycle(id));
+  }
+
+  // console.log(useSelector(state => state.reserveReducer))
   return (
-    <h1>Reserve Form</h1>
+    <div className="reserve-form">
+      {
+        sampleResponse ? (
+          <>
+          {
+                    sampleResponse.map((bike, key)=> {
+                      return (
+                        <div className="reserve-bike-wrap">
+                              <div className="div-div">
+                                <img src={bike.image} alt="" className="image-radio-bike"/>
+                                <h5 className='Title-reserve'>{bike.brand} {bike.model}</h5>
+                                <p>${bike.booking_fee}</p>
+                                <p className="year-reserve">Year: {bike.year}</p>
+                              </div>  
+                            <span className={bike.reserved ? "reserved-span":"reserved-span hidden"}>Reserved</span>
+                            <button
+                              type="button"
+                              className="reserve-reserve-button"
+                              onClick={() => reserve(bike.id)}
+                            >
+                              Reserve
+                            </button>
+                        </div>
+            
+                      )
+                    })
+          }
+          </>
+        ):
+        (<h2>...loading</h2>)
+      }
+    </div>
   );
 }
 
