8868a5fb557b669e820a2bb585e3cbb0a7caa211
camilevahviraki <camilevahviraki@gmail.com>
2022-09-23 00:52:30 +0200

    Update component/navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,34 +1,40 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import useAvatar from '../icons/avatar_account.png';
 import '../css/navbar.css';
 
 function Navbar() {
   return (
     <header>
-      <div className="navbar d-flex">
-        <div className="navTitle d-flex">
+      <div className="navbar">
+        <div className="navTitle">
           <h1>Bike Shop</h1>
         </div>
+        <div className='see-account-img-wrap'>
+          <NavLink to="user">
+            <img src={useAvatar} alt="" className='see-account-img' />
+          </NavLink>
+        </div>
 
-        <div className="navMenu d-flex">
+        <div className="navMenu">
           <NavLink to="/" className={({ isActive }) => (isActive ? 'rocket active' : 'rocket link')}>
-            <span>Motorcycles</span>
+            <span>MOTORCYCLES</span>
           </NavLink>
 
           <NavLink to="Reserve" className={({ isActive }) => (isActive ? 'rocket active' : 'rocket link')}>
-            <span>Reserve</span>
+            <span>RESERVE</span>
           </NavLink>
 
           <NavLink to="MyReservation" className={({ isActive }) => (isActive ? 'rocket active' : 'rocket link')}>
-            <span>My Reservations</span>
+            <span>MY RESERVATIONS</span>
           </NavLink>
 
           <NavLink to="AddItem" className={({ isActive }) => (isActive ? 'rocket active' : 'rocket link')}>
-            <span>Add Item</span>
+            <span>ADD ITEM</span>
           </NavLink>
 
           <NavLink to="RemoveItem" className={({ isActive }) => (isActive ? 'rocket active' : 'rocket link')}>
-            <span>Remove Item</span>
+            <span>REMOVE ITEM</span>
           </NavLink>
         </div>
       </div>
