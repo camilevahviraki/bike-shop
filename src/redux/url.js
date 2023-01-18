4c38902b046db2c116209732a9c82ee944eda80d
camilevahviraki <camilevahviraki@gmail.com>
2023-01-18 20:07:19 +0200

    update url

--- src/redux/url.js
+++ src/redux/url.js
@@ -1,4 +1,5 @@
-const linkURL = 'https://bike-shop-backend-r.onrender.com';
+const linkURL = 'https://bike-shop-backend.onrender.com';
 // const linkURL = 'http://localhost:3000';
+// const linkURL = 'http://0.0.0.0:3000';
 
 export default linkURL;
